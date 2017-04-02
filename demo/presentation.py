1b22a4e04cb57768bc3f7caa160eb3f2185ff867
Weilian Song <weiliansong@hotmail.com>
2017-04-02 17:30:38 -0400

    Started presentation demo script

---
+++ demo/presentation.py
@@ -0,0 +1,10 @@
+import ipdb
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def load_image():
+  img = mpimg.imread('stinkbug.png')
+  ipdb.set_trace()
+
+
